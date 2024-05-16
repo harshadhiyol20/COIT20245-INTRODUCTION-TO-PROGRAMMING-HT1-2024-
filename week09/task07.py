4f3ab1509187c3ddc4381ded6f3ffc0d2b34638a
harshadhiyol20 <162405631+harshadhiyol20@users.noreply.github.com>
2024-05-16 16:52:45 +1000

    Update task07.py

--- week09/task07.py
+++ week09/task07.py
@@ -1,1 +1,12 @@
+import requests
 
+def gps_coordinate(city):
+    url = f"https://nominatim.openstreetmap.org/search?q={city}&format=json"
+    response = requests.get(url)
+    data = response.json()
+    if data:
+        latitude = float(data[0]["lat"])
+        longitude = float(data[0]["lon"])
+        return {"latitude": latitude, "longitude": longitude}
+    else:
+        return None
