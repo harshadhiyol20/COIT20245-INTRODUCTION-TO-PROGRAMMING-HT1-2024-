34b188651f7f19acab0aec36fb18df5adad4e3b5
manavpatel20 <162406880+manavpatel20@users.noreply.github.com>
2024-05-18 00:46:56 +1000

    Update nominatim.py

--- nominatim.py
+++ nominatim.py
@@ -1,1 +1,23 @@
+import requests
 
+def gps_coordinate(city):
+    # url = "https://nominatim.openstreetmap.org/search?q=" + city + "&format=json"
+    url = f"https://us1.locationiq.com/v1/search?key=pk.2d900a55b458c544056d8ae89a373738&q={city}&format=json"
+    response = requests.get(url)
+    data = response.json()
+    # print(data)
+
+    if data:
+        latitude = float(data[0]['lat'])
+        longitude = float(data[0]['lon'])
+        return {"latitude": latitude, "longitude": longitude}
+    else:
+        return None
+
+# def test_gps_coordinate():
+#     coordinates = gps_coordinate("Brisbane")
+#     assert coordinates == {'latitude': -27.4689682, 'longitude': 153.0234991}
+#     print("Test passed for Brisbane")
+#     print(coordinates)
+
+# test_gps_coordinate()
