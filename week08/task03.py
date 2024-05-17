ddfdd0a9cfb734586062bffafbb24f35d87da1ac
harshadhiyol20 <162405631+harshadhiyol20@users.noreply.github.com>
2024-05-17 21:59:22 +1000

    Update task03.py

--- week08/task03.py
+++ week08/task03.py
@@ -1,3 +1,12 @@
+# Task-1 
+def display_menu():
+    print("\nCOIT20245 - Assignment 2")
+    print("\nMenu : ")
+    print("a. Help Menu")
+    print("b. Animal species in a city")
+    print("c. Exit")
+
+# Task-3
 def search_species(city):
     return [
         {"Species":{"TaxonID":1039,"AcceptedCommonName":"dolphin","PestStatus":"Nil"}},
@@ -12,3 +21,31 @@ def display_species(species_list):
             "TaxonID : " + str(species["Species"]["TaxonID"]) + "\n"
             "Name of the species : " + species["Species"]["AcceptedCommonName"] + "\n"
             "Pest Status : " + species["Species"]["PestStatus"] + "\n")
+
+# Task-2
+def main():
+  
+    display_menu()
+
+    while True:
+        user_input = input("wildlife > ")
+
+        if user_input.lower() == "help":
+            display_menu()
+        elif user_input.lower() == "exit":
+            print("Exiting the program.")
+            break
+        elif user_input.lower().startswith("species"):
+            parts = user_input.split(" ")
+            
+            if len(parts) != 2:
+                print("Error: Invalid command.")
+            else :
+                city = parts[1]
+                species_list = search_species
+        else:
+            print("Error: Invalid command. Please enter 'help' or 'exit'.")
+
+
+if name == "main":
+    main()
